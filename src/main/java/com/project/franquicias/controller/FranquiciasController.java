93c6e3f1eda354109dd49a03a8101786568e4bc3
@MontoyaSerna <lmparra89@gmail.com>
2024-12-08 17:00:27 -0500

    Creación Api select-all-franquicias

--- src/main/java/com/project/franquicias/controller/FranquiciasController.java
+++ src/main/java/com/project/franquicias/controller/FranquiciasController.java
@@ -1,5 +1,26 @@
 package com.project.franquicias.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.franquicias.service.FranquiciasService;
+
+@RestController
+@RequestMapping("/v1/api/franquicias")
 public class FranquiciasController {
+	
+	@Autowired
+	FranquiciasService franquiciaService;
+	
+	@GetMapping(value = "/select-all", 
+			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<?> selectAllFranquicias() {
+		return franquiciaService.selectAllFranquicias();
+
+	}
 
 }
