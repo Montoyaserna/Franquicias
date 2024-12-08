f327710f29044d1406e703bd6e0fe3649cdd21ed
@MontoyaSerna <lmparra89@gmail.com>
2024-12-08 17:01:27 -0500

    Creación representación tabla franquicia

--- src/main/java/com/project/franquicias/entity/FranquiciaEntity.java
+++ src/main/java/com/project/franquicias/entity/FranquiciaEntity.java
@@ -1,5 +1,28 @@
 package com.project.franquicias.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "franquicia")
 public class FranquiciaEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_franquicia", nullable = false)
+    private Long idFranquicia;
+	
+	@Column(name = "nombre_franquicia", nullable = false)
+    private String nombreFranquicia;
 
 }
